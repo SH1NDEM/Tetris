dad7d336cceb65207678ad7f275383fc04fc0834
SH1NDEM <willmirva@gmail.com>
2026-01-03 23:24:19 +0700

    случайный спавн фигур, множество фигур

--- Tetris/Application/Game.cs
+++ Tetris/Application/Game.cs
@@ -13,47 +13,48 @@ namespace Tetris.Application
         {
             _field = new GameField();
             _renderer = new ConsoleRenderer();
-
-            _figure = new Figure(FigureType.Z, 5, 19);
-            _field.FigureView(_figure);
             _field.ClearField();
-            _field.FigureView(_figure);
             _renderer.printGame();
+            
+        }
+
+        public Figure newFig()
+        {
+            Random rnd = new Random();
+
+            Array values = Enum.GetValues(typeof(FigureType));
+            FigureType randomType = (FigureType)values.GetValue(rnd.Next(values.Length));
+
+            return new Figure(randomType, 5, 15);
         }
 
         public async void Run()
         {
+            _figure = newFig();
+            bool isFall = true;
+
             DateTime lastFallTime = DateTime.Now;
+
             // Создаём renderer
             _renderer = new ConsoleRenderer();
 
-            _figure = new Figure(FigureType.Z, 5, 19);
-            //_field.FigureView(_figure);
-            //_field.ClearField();
-            //_renderer.printGame();
-
-            int Y = 19;
             while (true)
             {
                 DateTime now = DateTime.Now;
-                //_figure = new Figure(FigureType.Z, 5, 19);
 
                 if ((now - lastFallTime).TotalSeconds >= 0.5)
                 {
-                    
+
                     _figure.Y -= 1;
-                    //_field.FigureView(_figure);
-                    //_field.ClearField();
                     _field.FigureMove(_figure);
                     _field.FigureView(_figure);
                     _renderer.printGame();
                     lastFallTime = now;
-                    Y -= 1;
-                    
                 }
-                if (Y == 0 || (GameField.fieldMatrix[_figure.X, _figure.Y - 1]) == true)
+
+                if (_figure.Y == 1)
                 {
-                    _figure = new Figure(FigureType.Z, 5, 19);
+                    _figure = newFig();
                 }
             }
         }
