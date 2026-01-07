1c18b0cf7f4bb9dbe867ca1607e8eb90f5f50590
SHIDEN <willmirva@gmail.com>
2026-01-07 19:40:00 +0700

    feature(control): add figure control

---
+++ Tetris/Infrastructure/Control.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tetris.Core;
+
+namespace Tetris.Infrastructure
+{
+    internal class Control
+    {
+        public void figureControl(Figure figure) 
+        {
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        figure.X -= 1;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        figure.X += 1;
+                        break;
+                }
+            }
+        }
+    }
+}
