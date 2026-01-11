78f986de502e26506095d09effac6e71bff35287
SH1NDEM <willmirva@gmail.com>
2026-01-12 02:07:22 +0700

    глобальная проблема

--- Tetris/Infrastructure/ConsoleRenderer.cs
+++ Tetris/Infrastructure/ConsoleRenderer.cs
@@ -18,6 +18,32 @@ namespace Tetris.Infrastructure
         int startConsoleY = 0 +3;
         int viewFieldHight = GameField.fieldHight - 5;
 
+
+        // Проблема глобальная, нужно менять хранение фигуры в классе
+        void ViewNextFigure(Figure figure)
+        {
+            int[,] shape = FigureShapes.Shapes[figure.Type]; // НЕ крутится
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.SetCursorPosition(
+                    (shape[0, i] + 25) * 2,
+                    shape[1, i] + 6
+                );
+                Console.Write('#');
+            }
+        }
+
+
+        public void ClearNextFigure(Figure figure)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Console.SetCursorPosition((figure.Shape[0, i] + 25) * 2, figure.Shape[1, i] + 6);
+                Console.Write(' ');
+            }
+        }
+
         void printBorders()
         {
             Console.SetCursorPosition(startConsoleX, startConsoleY);
@@ -62,12 +88,13 @@ namespace Tetris.Infrastructure
             }
         }
 
-        public void printGame()
+        public void printGame(Figure figure)
         {
             Console.CursorVisible = false;
             printBorders();
             printBottom();
             printField();
+            ViewNextFigure(figure);
         }
     }
 }
